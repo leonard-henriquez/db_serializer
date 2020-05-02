f76bc47c2f0d10ab6e926e42d31398f91c19e179
Leonard Henriquez <leonard@flatlooker.com>
2020-05-02 19:22:19 +0200

    Cleaning

--- lib/active_geo/geo_utilities.rb
+++ lib/active_geo/geo_utilities.rb
@@ -3,34 +3,32 @@
 module ActiveGeo
   # Module defining geographic tools
   module GeoUtilities
-    CONFIG = Rails.application.config
-
     # SQL utilies for spatial queries
     module SQL
       class << self
-        # returns a string containing part of a SQL query to get a field named `geo_json`
-        # this field is a GeoJSON representation of the `geometry_column`
-        # the GeoJSON will contain the specified `columns` in its properties
-        # this method should be used in a SELECT statement
+        # Returns a string containing part of a SQL query to get a field named `geo_json`.
+        # This field is a GeoJSON representation of the `geometry_column`.
+        # The GeoJSON will contain the specified `columns` in its properties.
+        # This method should be used in a SELECT statement.
         #
-        # example with sql:
+        # Example with sql:
         #     ```ruby
         #       ActiveRecord::Base.connection.exec_query(
         #         "SELECT id, geometry, #{geo_json_field([:id])} FROM table"
         #       )
         #     ````
-        #     it will return results with three columns `id`, `geometry` and `geo_json`
-        #     the `geo_json` field will be string containing a valid GeoJSON as the example below:
+        #     It will return results with three columns `id`, `geometry` and `geo_json`.
+        #     The `geo_json` field will be string containing a valid GeoJSON as the example below:
         #     {\"id\": 1, \"type\": \"Feature\", \"geometry\": {\"type\": \"MultiLineString\",
         #       \"coordinates\": [[[x,y]]]}, \"properties\": {\"id\": 555, \"url\": \"paris-75000\"}}
         #
-        # example with an ActiveRecord::Base:
+        # Example with an ActiveRecord::Base:
         #     ```ruby
         #       record = Model.select(geo_json_field([:id]))
         #       record.geo_json
         #     ```
-        #     this will return a hash of the GeoJSON because active record automatically casts it
-        #     to prevent this behaviour use `record.geo_json_before_type_cast` instead
+        #     This will return a hash of the GeoJSON because active record automatically casts it.
+        #     To prevent this behaviour use `record.geo_json_before_type_cast` instead
         def geo_json_field(columns, opts = {})
           opts = {
             geometry_column: :geometry,
@@ -54,13 +52,13 @@ module ActiveGeo
           SQL
         end
 
-        # returns a string containing an entire SQL query that returns one line with one field
-        # the field is named `json` and contains a FeatureCollection
+        # Returns a string containing an entire SQL query that returns one line with one field.
+        # The field is named `json` and contains a FeatureCollection.
         # FeatureCollection specs: http://wiki.geojson.org/GeoJSON_draft_version_6#FeatureCollection
         #
-        # the features contained in the feature collection are provided by the `features` parameter
-        # `features` can be a raw sql query or an ActiveRecord_Relation
-        # example:
+        # The features contained in the feature collection are provided by the `features` parameter
+        # `features` can be a raw sql query or an ActiveRecord_Relation.
+        # Example:
         #     features = Model.select(geo_json_field([:id]))
         #     query = feature_collection(features)
         #     ActiveRecord::Base.connection.exec_query(query)
@@ -78,13 +76,13 @@ module ActiveGeo
     end
 
     class << self
-      # returns a RGeo::Cartesian::BoundingBox based on min_lng, min_lat, max_lng, max_lat
-      def bounding_box(params)
-        return nil if %i[min_lng min_lat max_lng max_lat].any? { |key| params[key].nil? }
+      # Returns a RGeo::Cartesian::BoundingBox based on min_lng, min_lat, max_lng, max_lat.
+      def bounding_box(box, srid = 4326)
+        return nil if %i[min_lng min_lat max_lng max_lat].any? { |key| box[key].nil? }
 
-        factory = RGeo::Geographic.spherical_factory(srid: CONFIG.srid)
-        min = factory.point(params[:min_lng], params[:min_lat])
-        max = factory.point(params[:max_lng], params[:max_lat])
+        factory = RGeo::Geographic.spherical_factory(srid: srid)
+        min = factory.point(box[:min_lng], box[:min_lat])
+        max = factory.point(box[:max_lng], box[:max_lat])
 
         RGeo::Cartesian::BoundingBox.create_from_points(min, max)
       end
