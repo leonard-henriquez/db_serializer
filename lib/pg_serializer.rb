97ffc4c2d615fe2df614192703ba9349e9e3cc2e
Leonard Henriquez <leonard@flatlooker.com>
2020-05-09 17:33:02 +0200

    Update doc

--- lib/pg_serializer.rb
+++ lib/pg_serializer.rb
@@ -3,6 +3,10 @@
 require 'active_support'
 require 'active_record'
 
+##
+# Module that contains spatial serializers for ActiveRecord models.
+# The extended class must be an ActiveRecord::Base.
+# It must also have a geometry column (the name of this column can be customized).
 module PgSerializer
   extend ActiveSupport::Autoload
 
